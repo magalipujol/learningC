11f25da8ad1d363b9305d83d11d508adb181d6db
magalicpujol <magalicpujol@gmail.com>
2023-07-29 09:53:02 -0300

    lista simplemente enlazada
    Signed-off-by: magalicpujol <magalicpujol@gmail.com>

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#include <string.h>
+
+struct Persona {   // Structure declaration
+  int Edad;           // Member (int variable)
+  char Nombre[20];       // Member (char variable)
+};
+
+struct Nodo {
+  int Valor;
+  struct Nodo* siguiente;
+};
+
+void imprimir(struct Nodo* nodo)
+{
+  printf("%d", nodo->Valor);
+  if (nodo->siguiente != NULL)
+  {
+    printf("%d", nodo->siguiente->Valor);
+  }
+}
+
+int main()
+{
+  struct Persona persona1;
+  persona1.Edad = 20;
+  strcpy(persona1.Nombre, "Juan");
+  // printf("La edad de %s es %d", persona1.Nombre, persona1.Edad);
+  struct Persona* personaPuntero;
+  // personaPuntero = &persona1;
+
+  struct Nodo nodo1;
+  struct Nodo* nodo2;
+  nodo2 = malloc(sizeof(struct Nodo));
+  nodo1.Valor = 1;
+  nodo1.siguiente = nodo2;
+  nodo2->Valor = 2;
+  nodo2->siguiente = NULL;
+
+  imprimir(&nodo1);
+  // printf("La edad de %s es %d", personaPuntero->Nombre, personaPuntero->Edad);
+}
